021a7b0561e8f4e36100b6a98ea1f8747111e274
Gary Talent <gary@drinkingtea.net>
2023-01-15 16:52:25 -0600

    Add buildcore

---
+++ deps/buildcore/base.mk
@@ -0,0 +1,173 @@
+#
+#  Copyright 2016 - 2021 gary@drinkingtea.net
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+ifeq (${OS},Windows_NT)
+	SHELL := powershell.exe
+	.SHELLFLAGS := -NoProfile -Command
+	OS=windows
+	HOST_ENV=${OS}
+else
+	OS=$(shell uname | tr [:upper:] [:lower:])
+	HOST_ENV=${OS}-$(shell uname -m)
+endif
+
+DEVENV=devenv$(shell pwd | sed 's/\//-/g')
+DEVENV_IMAGE=${PROJECT_NAME}-devenv
+ifneq ($(shell which docker 2> /dev/null),)
+	ifeq ($(shell docker inspect --format="{{.State.Status}}" ${DEVENV} 2>&1),running)
+		ENV_RUN=docker exec -i -t --user $(shell id -u ${USER}) ${DEVENV}
+	endif
+endif
+
+ifneq ($(shell ${ENV_RUN} which python3 2> /dev/null),)
+	PYTHON3=python3
+else
+	ifeq ($(shell ${ENV_RUN} python -c 'import sys; print(sys.version_info[0])'),3)
+		PYTHON3=python
+	endif
+endif
+
+SCRIPTS=${BUILDCORE_PATH}/scripts
+SETUP_BUILD=${PYTHON3} ${SCRIPTS}/setup-build.py
+PYBB=${PYTHON3} ${SCRIPTS}/pybb.py
+CMAKE_BUILD=${PYBB} cmake-build
+GET_ENV=${PYBB} getenv
+CTEST=${PYBB} ctest-all
+RM_RF=${PYBB} rm
+HOST=$(shell ${PYBB} hostname)
+BUILDCORE_HOST_SPECIFIC_BUILDPATH=$(shell ${GET_ENV} BUILDCORE_HOST_SPECIFIC_BUILDPATH)
+ifneq (${BUILDCORE_HOST_SPECIFIC_BUILDPATH},)
+BUILD_PATH=build/${HOST}
+else
+BUILD_PATH=build
+endif
+ifdef USE_VCPKG
+	ifndef VCPKG_DIR_BASE
+		VCPKG_DIR_BASE=.vcpkg
+	endif
+	ifndef VCPKG_VERSION
+		VCPKG_VERSION=2020.06
+	endif
+	VCPKG_TOOLCHAIN=--toolchain=${VCPKG_DIR}/scripts/buildsystems/vcpkg.cmake
+endif
+ifeq ($(OS),darwin)
+	DEBUGGER=lldb --
+else
+	DEBUGGER=gdb --args
+endif
+
+VCPKG_DIR=$(VCPKG_DIR_BASE)/$(VCPKG_VERSION)-$(HOST_ENV)
+CURRENT_BUILD=$(HOST_ENV)-$(shell ${ENV_RUN} ${PYBB} cat .current_build)
+
+.PHONY: build
+build:
+	${ENV_RUN} ${CMAKE_BUILD} ${BUILD_PATH}
+.PHONY: install
+install:
+	${ENV_RUN} ${CMAKE_BUILD} ${BUILD_PATH} install
+.PHONY: clean
+clean:
+	${ENV_RUN} ${CMAKE_BUILD} ${BUILD_PATH} clean
+.PHONY: purge
+purge:
+	${ENV_RUN} ${RM_RF} .current_build
+	${ENV_RUN} ${RM_RF} ${BUILD_PATH}
+	${ENV_RUN} ${RM_RF} dist
+.PHONY: test
+test: build
+	${ENV_RUN} mypy ${SCRIPTS}
+	${ENV_RUN} ${CMAKE_BUILD} ${BUILD_PATH} test
+.PHONY: test-verbose
+test-verbose: build
+	${ENV_RUN} ${CTEST} ${BUILD_PATH} --output-on-failure
+.PHONY: test-rerun-verbose
+test-rerun-verbose: build
+	${ENV_RUN} ${CTEST} ${BUILD_PATH} --rerun-failed --output-on-failure
+
+.PHONY: devenv-image
+devenv-image:
+	docker build . -t ${DEVENV_IMAGE}
+.PHONY: devenv-create
+devenv-create:
+	docker run -d \
+		-e LOCAL_USER_ID=$(shell id -u ${USER}) \
+		-e DISPLAY=$(DISPLAY) \
+		-e QT_AUTO_SCREEN_SCALE_FACTOR=1 \
+		-v /tmp/.X11-unix:/tmp/.X11-unix \
+		-v /run/dbus/:/run/dbus/ \
+		-v $(shell pwd):/usr/src/project \
+		-v /dev/shm:/dev/shm \
+		--restart=always \
+		--name ${DEVENV} \
+		-t ${DEVENV_IMAGE} bash
+.PHONY: devenv-destroy
+devenv-destroy:
+	docker rm -f ${DEVENV}
+ifdef ENV_RUN
+.PHONY: devenv-shell
+devenv-shell:
+	${ENV_RUN} bash
+endif
+
+ifdef USE_VCPKG
+
+.PHONY: vcpkg
+vcpkg: ${VCPKG_DIR} vcpkg-install
+
+${VCPKG_DIR}:
+	${ENV_RUN} ${RM_RF} ${VCPKG_DIR}
+	${ENV_RUN} mkdir -p ${VCPKG_DIR_BASE}
+	${ENV_RUN} git clone -b release --depth 1 --branch ${VCPKG_VERSION} https://github.com/microsoft/vcpkg.git ${VCPKG_DIR}
+ifneq (${OS},windows)
+	${ENV_RUN} ${VCPKG_DIR}/bootstrap-vcpkg.sh
+else
+	${ENV_RUN} ${VCPKG_DIR}/bootstrap-vcpkg.bat
+endif
+
+.PHONY: vcpkg-install
+vcpkg-install:
+ifneq (${OS},windows)
+	${VCPKG_DIR}/vcpkg install ${VCPKG_PKGS}
+else
+	${VCPKG_DIR}/vcpkg install --triplet x64-windows ${VCPKG_PKGS}
+endif
+
+else ifdef USE_CONAN # USE_VCPKG ################################################
+
+.PHONY: setup-conan
+conan-config:
+	${ENV_RUN} conan profile new ${PROJECT_NAME} --detect --force
+ifeq ($(OS),linux)
+	${ENV_RUN} conan profile update settings.compiler.libcxx=libstdc++11 ${PROJECT_NAME}
+else
+	${ENV_RUN} conan profile update settings.compiler.cppstd=20 ${PROJECT_NAME}
+ifeq ($(OS),windows)
+	${ENV_RUN} conan profile update settings.compiler.runtime=static ${PROJECT_NAME}
+endif
+endif
+.PHONY: conan
+conan:
+	${ENV_RUN} ${PYBB} conan-install ${PROJECT_NAME}
+endif # USE_VCPKG ###############################################
+
+.PHONY: configure-xcode
+configure-xcode:
+	${ENV_RUN} ${SETUP_BUILD} ${VCPKG_TOOLCHAIN} --build_tool=xcode --current_build=0 --build_root=${BUILD_PATH}
+
+.PHONY: configure-release
+configure-release:
+	${ENV_RUN} ${SETUP_BUILD} ${VCPKG_TOOLCHAIN} --build_type=release --build_root=${BUILD_PATH}
+
+.PHONY: configure-debug
+configure-debug:
+	${ENV_RUN} ${SETUP_BUILD} ${VCPKG_TOOLCHAIN} --build_type=debug --build_root=${BUILD_PATH}
+
+.PHONY: configure-asan
+configure-asan:
+	${ENV_RUN} ${SETUP_BUILD} ${VCPKG_TOOLCHAIN} --build_type=asan --build_root=${BUILD_PATH}
+
---
+++ deps/buildcore/scripts/pybb.py
@@ -0,0 +1,139 @@
+#! /usr/bin/env python3
+
+#
+#  Copyright 2016 - 2021 gary@drinkingtea.net
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+# "Python Busy Box" - adds cross-platform equivalents to Unix commands that
+#                     don't translate well to that other operating system
+
+import os
+import platform
+import shutil
+import subprocess
+import sys
+from typing import List, Optional
+
+
+def mkdir(path: str):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+# this exists because Windows is utterly incapable of providing a proper rm -rf
+def rm(path: str):
+    if (os.path.exists(path) or os.path.islink(path)) and not os.path.isdir(path):
+        os.remove(path)
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+
+
+def ctest_all() -> int:
+    base_path = sys.argv[2]
+    if not os.path.isdir(base_path):
+        # no generated projects
+        return 0
+    args = ['ctest'] + sys.argv[3:]
+    orig_dir = os.getcwd()
+    for d in os.listdir(base_path):
+        os.chdir(os.path.join(orig_dir, base_path, d))
+        err = subprocess.run(args).returncode
+        if err != 0:
+            return err
+    return 0
+
+
+def cmake_build(base_path: str, target: Optional[str]) -> int:
+    if not os.path.isdir(base_path):
+        # nothing to build
+        return 0
+    for d in os.listdir(base_path):
+        args = ['cmake', '--build', os.path.join(base_path, d)]
+        if target is not None:
+            args.extend(['--target', target])
+        err = subprocess.run(args).returncode
+        if err != 0:
+            return err
+    return 0
+
+
+def conan() -> int:
+    project_name = sys.argv[2]
+    conan_dir = '.conanbuild'
+    err = 0
+    try:
+        mkdir(conan_dir)
+    except:
+        return 1
+    if err != 0:
+        return err
+    args = ['conan', 'install', '../', '--build=missing', '-pr', project_name]
+    os.chdir(conan_dir)
+    err = subprocess.run(args).returncode
+    if err != 0:
+        return err
+    return 0
+
+
+def cat(paths: List[str]) -> int:
+    for path in paths:
+        try:
+            with open(path) as f:
+                data = f.read()
+                sys.stdout.write(data)
+        except FileNotFoundError:
+            sys.stderr.write('cat: {}: no such file or directory\n'.format(path))
+            return 1
+    sys.stdout.write('\n')
+    return 0
+
+
+def get_env(var_name: str) -> int:
+    if var_name not in os.environ:
+        return 1
+    sys.stdout.write(os.environ[var_name])
+    return 0
+
+
+def hostname() -> int:
+    sys.stdout.write(platform.node())
+    return 0
+
+
+def main() -> int:
+    err = 0
+    if sys.argv[1] == 'mkdir':
+        try:
+            mkdir(sys.argv[2])
+        except:
+            err = 1
+    elif sys.argv[1] == 'rm':
+        for i in range(2, len(sys.argv)):
+            rm(sys.argv[i])
+    elif sys.argv[1] == 'conan-install':
+        err = conan()
+    elif sys.argv[1] == 'ctest-all':
+        err = ctest_all()
+    elif sys.argv[1] == 'cmake-build':
+        err = cmake_build(sys.argv[2], sys.argv[3] if len(sys.argv) > 3 else None)
+    elif sys.argv[1] == 'cat':
+        err = cat(sys.argv[2:])
+    elif sys.argv[1] == 'getenv':
+        err = get_env(sys.argv[2])
+    elif sys.argv[1] == 'hostname':
+        err = hostname()
+    else:
+        sys.stderr.write('Command not found\n')
+        err = 1
+    return err
+
+
+if __name__ == '__main__':
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        sys.exit(1)
